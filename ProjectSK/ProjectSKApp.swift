51c1fb0193bf928ff43b3602a70e50474b6f5a74
suryav27 <160803358+suryav27@users.noreply.github.com>
2025-04-17 19:52:44 -0500

    Initial Commit

---
+++ ProjectSK/ProjectSKApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSKApp.swift
+//  ProjectSK
+//
+//  Created by Surya Venigalla on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
